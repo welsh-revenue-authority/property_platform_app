0719a0dbc2b6660e9999c566729e845acd31c258
Richard Pope <richard@richardpope.org>
2022-03-09 16:32:24 +0000

    landing page

---
+++ README.md
@@ -0,0 +1,13 @@
+## Install
+
+venv .
+source bin/activate
+
+pip install -r requirements.txt
+
+
+## Run
+
+venv .
+source bin/activate
+uvicorn main:app --reload
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+#from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+#security = HTTPBasic()
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+        return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/api")
 def read_api_root():
