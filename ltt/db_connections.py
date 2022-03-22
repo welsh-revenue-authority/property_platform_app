20f53a55c9fc0ddf14b90503f640e23015ec66d5
Tom Greenwood <thomas.greenwood@kainos.com>
2022-03-22 14:49:21 +0000

    added sql_command() and seperated connection into dedicated function

--- ltt/db_connections.py
+++ ltt/db_connections.py
@@ -2,7 +2,8 @@ import psycopg2
 import os
 
 
-def sql_query(query: str):
+def connect():
+    """Returns connection"""
     connection = psycopg2.connect(
         user=os.environ.get("HEROKU_PGUSERNAME"),
         password=os.environ.get("HEROKU_PGPASSWORD"),
@@ -10,12 +11,26 @@ def sql_query(query: str):
         port="5432",
         database=os.environ.get("HEROKU_PGDATABASE"),
     )
+    return connection
+
+
+def sql_query(query: str):
+    connection = connect()
     cursor = connection.cursor()
     cursor.execute(query)
     result = cursor.fetchall()
     connection.close()
 
     return result
+
+
+def sql_command(command: str) -> None:
+    """Executes command e.g. insert data"""
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute(command)
+    connection.commit()
+    connection.close()
 
 
 if __name__ == "__main__":
