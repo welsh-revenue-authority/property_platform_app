24e6c2c61cd1530d89013fc02834a4bc6db9a1fb
Tom Greenwood <thomas.greenwood@kainos.com>
2022-03-25 15:58:25 +0000

    formating

--- ltt/create_attribute_type.py
+++ ltt/create_attribute_type.py
@@ -1,5 +1,5 @@
-import argparse
 from ltt.db_connections import sql_command
+
 
 def create_attribute(attribute_type: str, description: str, data_type: str):
     """Adds a new attribute type to data base"""
@@ -9,7 +9,7 @@ def create_attribute(attribute_type: str, description: str, data_type: str):
     """
     print(query)
     sql_command(query)
-    
+
     print(f"new attribute '{attribute_type}' added to database")
 
 
