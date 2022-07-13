a4f967fa7ddb62f409e3050d37997ed095f4023d
Luke Sawicki <luke.sawicki.it@gmail.com>
2022-07-13 12:05:42 +0100

    ltt tax rates

--- ltt/ltt_rate.py
+++ ltt/ltt_rate.py
@@ -9,8 +9,8 @@ def land_transaction_tax_rate_query(geography_id: str):
     """Returns land transaction tax rate"""
     result = sql_query(
         f"""
-        SELECT id, description_en, description_cy, "lower-threshold", "upper-threshold", rate, "land-transaction-tax-category", "geography-id", organisation, "start-date", "end-date"
-	FROM public."land-transaction-tax-rate" WHERE "start-date"<CURRENT_DATE AND ("end-date">CURRENT_DATE OR "end-date" is NULL) AND "geography-id"='{geography_id}';
+        SELECT id, description_en, description_cy, "lower_threshold", "upper_threshold", rate, "land_transaction_tax_category", "geography_id", organisation, "start_date", "end_date"
+	FROM public."land_transaction_tax_rate" WHERE "start_date"<CURRENT_DATE AND ("end_date">CURRENT_DATE OR "end_date" is NULL) AND "geography_id"='{geography_id}';
     """
     )
     return result
